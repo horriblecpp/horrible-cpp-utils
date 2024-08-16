f78038bd7de47dceb553346f4df06f4589ea69c4
horriblecpp <horribleccprogram@proton.me>
2024-08-16 13:34:07 -0400

    horrible::utils::IsCallableT<> SFINAE-based trait

--- include/HorribleUtils/horrible_type_traits.hpp
+++ include/HorribleUtils/horrible_type_traits.hpp
@@ -2,7 +2,7 @@
 #define HORRIBLE_UTILS_TYPE_TRAITS_HPP_
 
 #include <iostream>
-
+#include <type_traits>      // std::is_same<>, std::is_same_v<>
 
 namespace horrible::utils
 {
@@ -14,6 +14,23 @@ template<typename T> constexpr char const* print_expr_category<T&&> {"xvalue"};
 
 // Macro isn't affected by namespaces
 #define SHOW(expr) std::cout << #expr ": " << print_expr_category<decltype((expr)) << std::endl
+
+
+
+
+
+template<typename T>
+class IsCallableT {
+public:
+  static constexpr bool value = std::is_same_v<char, decltype(test<T>(nullptr))>;
+private:
+  template<typename U, typename = decltype(std::declval<U>()())> static char test(char const *);
+  template<typename> static long test(...);
+};
+
+// V = ::value, nothing = ::type, T = type trait.
+template<typename T>
+constexpr bool IsCallableV = IsCallableT<T>::value;
 
 
 
